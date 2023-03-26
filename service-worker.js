e644bae6a45c81340664754492c7575ffbc9b100
danehobrecht <76x5l22l@anonaddy.me>
2023-03-26 09:28:27 -0700

    modified:   service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -20,9 +20,9 @@ const RUNTIME = 'runtime';
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
 	'index.html',
-	'./', // Alias for index.html
+	'./index.html', // Alias for index.html
 	'styles.css',
-	'./'
+	'./styles.css'
 ];
 
 // The install handler takes care of precaching the resources we always need.
