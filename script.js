2670028b13159bedf242c1542f2c37e41627fde8
danehobrecht <76x5l22l@anonaddy.me>
2023-04-03 22:17:58 -0700

    modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+if ('serviceWorker' in navigator) { navigator.serviceWorker.register('service-worker.js'); }
+
 $(document).ready(function(){ 
 	$('.dots a').click(function(event) { 
 		$(this).attr('id', 'active').siblings().attr('id', 'inactive');
