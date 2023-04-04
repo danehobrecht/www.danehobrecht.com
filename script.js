0f36a3bc02b5f93c6ea643d2eb4f8dcbbab45730
danehobrecht <76x5l22l@anonaddy.me>
2023-04-03 21:42:44 -0700

    deleted:    assets/fonts/liberation-sans/AUTHORS 	deleted:    assets/fonts/liberation-sans/COPYING 	deleted:    assets/fonts/liberation-sans/ChangeLog 	deleted:    assets/fonts/liberation-sans/LiberationSans-Bold.ttf 	deleted:    assets/fonts/liberation-sans/LiberationSans-BoldItalic.ttf 	deleted:    assets/fonts/liberation-sans/LiberationSans-Italic.ttf 	deleted:    assets/fonts/liberation-sans/LiberationSans-Regular.ttf 	deleted:    assets/fonts/liberation-sans/License.txt 	deleted:    assets/fonts/liberation-sans/README 	deleted:    assets/fonts/liberation-serif/LiberationSerif-Bold.ttf 	deleted:    assets/fonts/liberation-serif/LiberationSerif-BoldItalic.ttf 	deleted:    assets/fonts/liberation-serif/LiberationSerif-Italic.ttf 	deleted:    assets/fonts/liberation-serif/LiberationSerif-Regular.ttf 	deleted:    assets/fonts/liberation-serif/SIL Open Font License.txt 	modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-$(document).ready(function(){ $('.dots a').click(function(event){ $(this).attr('id', 'active').siblings().attr('id', 'inactive'); }); });
+$(document).ready(function(){ 
+	$('.dots a').click(function(event) { 
+		$(this).attr('id', 'active').siblings().attr('id', 'inactive');
+		$(event.currentTarget).attr('id', 'active').siblings().attr('id', 'inactive');
+	});
+});
