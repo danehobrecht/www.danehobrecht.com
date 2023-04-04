585009dce53fa5e4e96b6ee986d080e31aae52a4
danehobrecht <76x5l22l@anonaddy.me>
2023-04-04 13:27:55 -0700

    modified:   assets/documents/dane-hobrecht-resume-001.pdf 	modified:   assets/documents/sources/dane-hobrecht-resume-001.pdf 	modified:   script.js

--- script.js
+++ script.js
@@ -3,6 +3,5 @@ if ('serviceWorker' in navigator) { navigator.serviceWorker.register('service-wo
 $(document).ready(function(){ 
 	$('.dots a').click(function(event) { 
 		$(this).attr('id', 'active').siblings().attr('id', 'inactive');
-		$(event.currentTarget).attr('id', 'active').siblings().attr('id', 'inactive');
 	});
 });
