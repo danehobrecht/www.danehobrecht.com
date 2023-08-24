0f83bb6d66492994883a6cc5c3b6ab865c7ad45f
danehobrecht <76x5l22l@anonaddy.me>
2023-08-23 21:46:45 -0700

    modified:   script.js

--- script.js
+++ script.js
@@ -1,24 +1,13 @@
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('service-worker.js');
+    navigator.serviceWorker.register('service-worker.js');
 }
 
-$(document).ready(function(){
+$(document).ready(function() {
 	// Style active and inactive navigation dots appropriately 
 	$('.dots a').click(function(event) { 
+		event.preventDefault();
 		$(this).attr('id', 'active').siblings().attr('id', 'inactive');
-	});
-
-	document.addEventListener("DOMContentLoaded", function() {
-		const navLinks = document.querySelectorAll('.dots a[href^="#"]'); // Select all navigation links
-
-		// Loop through each navigation link and add a click event listener
-		navLinks.forEach(link => {
-			link.addEventListener('click', function(event) {
-				event.preventDefault(); // Prevent the default link behavior
-
-				const targetId = link.getAttribute('href').replace('#', ''); // Get the target ID
-				window.location.href = targetId; // Update the URL without the "#" symbol
-			});
-		});
+		const targetId = $(this).attr('href').replace('#', '');
+		window.location.hash = targetId;
 	});
 });
