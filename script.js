50e67e8cf11c51a62586c37c4c93d9516876edb6
danehobrecht <76x5l22l@anonaddy.me>
2023-08-23 21:53:25 -0700

    modified:   script.js

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-// if ('serviceWorker' in navigator) {
-// 	navigator.serviceWorker.register('service-worker.js');
-// }
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('service-worker.js');
+}
 
-$(document).ready(function() {
-	// Style active and inactive navigation dots appropriately 
+$(document).ready(function(){
+	// Style active and inactive navigation dots accordingly 
 	$('.dots a').click(function(event) { 
-		event.preventDefault();
 		$(this).attr('id', 'active').siblings().attr('id', 'inactive');
-		const targetId = $(this).attr('href').replace('#', ''); // Get the target ID
-		window.history.pushState({}, '', targetId); // Update the URL without the "#" symbol
 	});
 });
