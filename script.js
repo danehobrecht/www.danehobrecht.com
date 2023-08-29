5af0a1702f48243537b008f6b1ae3138bf9b94d8
danehobrecht <76x5l22l@anonaddy.me>
2023-08-29 13:38:43 -0700

    Horizontal scroll functionality

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
 // Install service worker
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('service-worker.js');
-}
+if ('serviceWorker' in navigator) { navigator.serviceWorker.register('service-worker.js'); }
 
-$(document).ready(function(){
-	// Style active and inactive navigation dots accordingly 
-	$('.dots a').click(function(event) { 
-		$(this).attr('id', 'active').siblings().attr('id', 'inactive');
+// Navigation dots
+$(document).ready(function() {
+	const scrollContainer = $('#resume');
+	const dots = $('.dots a');
+	let moduleWidth = calculateModuleWidth();
+
+	scrollContainer.on('scroll', function() {
+		const scrollPosition = scrollContainer.scrollLeft();
+		const containerWidth = scrollContainer.width();
+		const snappedModuleIndex = Math.floor(scrollPosition / moduleWidth);
+		const middleModuleIndex = Math.floor(snappedModuleIndex + containerWidth / (2 * moduleWidth));
+		updateActiveDot(middleModuleIndex);
 	});
+
+	let isDragging = false;
+	let startX, scrollLeft;
+
+	scrollContainer.mousedown(function(e) {
+		isDragging = true;
+		startX = e.pageX - scrollContainer.offset().left;
+		scrollLeft = scrollContainer.scrollLeft();
+	});
+
+	$(document).mousemove(function(e) {
+		if (!isDragging) return;
+		e.preventDefault();
+		const x = e.pageX - scrollContainer.offset().left;
+		const walk = (x - startX) * 4;
+		scrollContainer.scrollLeft(scrollLeft - walk);
+	});
+
+	$(document).mouseup(function() {
+		isDragging = false;
+	}).mouseleave(function() {
+		isDragging = false;
+	});
+
+	function calculateModuleWidth() { return $('.module').eq(0).width(); }
+
+	function updateActiveDot(activeIndex) {
+		dots.removeAttr('id');
+		dots.eq(activeIndex).attr('id', 'active');
+	}
 });
