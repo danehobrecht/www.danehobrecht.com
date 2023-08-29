779ea9d2de4de689e8c6388a387d6e713aae4b94
danehobrecht <76x5l22l@anonaddy.me>
2023-08-29 14:37:27 -0700

    modified:   script.js

--- script.js
+++ script.js
@@ -16,19 +16,18 @@ $(document).ready(function() {
 			const containerWidth = scrollContainer.width();
 			const snappedModuleIndex = Math.floor(scrollPosition / moduleWidth);
 			const middleModuleIndex = Math.floor(snappedModuleIndex + containerWidth / (2 * moduleWidth));
-		
 			updateActiveDot(middleModuleIndex);
 		});
 	
 		let isDragging = false;
 		let startX, scrollLeft;
-	
+
 		scrollContainer.on('mousedown', function(e) {
 			isDragging = true;
 			startX = e.clientX - scrollContainer.offset().left;
 			scrollLeft = scrollContainer.scrollLeft();
 		});
-	
+
 		$(document).on('mousemove', function(e) {
 			if (!isDragging) return;
 			e.preventDefault();
@@ -36,19 +35,19 @@ $(document).ready(function() {
 			const walk = (x - startX) * 4;
 			scrollContainer.scrollLeft(scrollLeft - walk);
 		});
-	
+
 		$(document).on('mouseup', function() {
 			isDragging = false;
 		}).on('mouseleave', function() {
 			isDragging = false;
 		});
-	
+
 		function calculateModuleWidth() {
 			const width = $('.module').eq(0).width();
 			console.log('Module Width:', width); // Added console log
 			return width;
 		}
-	
+
 		function updateActiveDot(activeIndex) {
 			dots.removeAttr('id');
 			dots.eq(activeIndex).attr('id', 'active');
