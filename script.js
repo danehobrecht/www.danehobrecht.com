cde14bb6d42f92abd4585933807c7671afe43ff8
danehobrecht <76x5l22l@anonaddy.me>
2023-08-31 17:10:35 -0700

    modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-$(document).ready(function() {
-	// Install service worker
+$(document).ready(() => {
 	if ('serviceWorker' in navigator) {
 		navigator.serviceWorker.register('service-worker.js');
 	}
 
-	// Navigation dots
 	const scrollContainer = $('#resume');
 	const dots = $('.dots a');
+	let moduleWidth = calculateModuleWidth();
+	let isDragging = false, startX, scrollLeft;
 
-	$(window).on('load', function() {
-		let moduleWidth = calculateModuleWidth();
-
-		scrollContainer.on('scroll', function() {
+	$(window).on('load', () => {
+		scrollContainer.on('scroll', () => {
 			const scrollPosition = scrollContainer.scrollLeft();
 			const containerWidth = scrollContainer.width();
 			const snappedModuleIndex = Math.floor(scrollPosition / moduleWidth);
@@ -19,16 +17,13 @@ $(document).ready(function() {
 			updateActiveDot(middleModuleIndex);
 		});
 
-		let isDragging = false;
-		let startX, scrollLeft;
-
-		scrollContainer.on('mousedown', function(e) {
+		scrollContainer.on('mousedown touchstart', (e) => {
 			isDragging = true;
 			startX = e.clientX - scrollContainer.offset().left;
 			scrollLeft = scrollContainer.scrollLeft();
 		});
 
-		$(document).on('mousemove', function(e) {
+		$(document).on('mousemove touchmove', (e) => {
 			if (!isDragging) return;
 			e.preventDefault();
 			const x = e.clientX - scrollContainer.offset().left;
@@ -36,20 +31,15 @@ $(document).ready(function() {
 			scrollContainer.scrollLeft(scrollLeft - walk);
 		});
 
-		$(document).on('mouseup', function() {
-			isDragging = false;
-		}).on('mouseleave', function() {
-			isDragging = false;
-		});
-
-		function calculateModuleWidth() {
-			const width = $('.module').eq(0).width();
-			return width;
-		}
-
-		function updateActiveDot(activeIndex) {
-			dots.removeAttr('id');
-			dots.eq(activeIndex).attr('id', 'active');
-		}
+		$(document).on('mouseup mouseleave touchend', () => { isDragging = false; });
 	});
+
+	function calculateModuleWidth() {
+		return $('.module').eq(0).width();
+	}
+
+	function updateActiveDot(activeIndex) {
+		dots.removeAttr('id');
+		dots.eq(activeIndex).attr('id', 'active');
+	}
 });
