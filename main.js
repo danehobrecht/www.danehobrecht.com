237d0fbd62b1e6e9fc2336b53df9a8ed241ccab5
danehobrecht <76x5l22l@anonaddy.me>
2023-09-02 18:21:36 -0700

    modified:   index.html 	modified:   main.js 	modified:   styles.css

--- main.js
+++ main.js
@@ -1,11 +1,59 @@
+// Flyer navigation
 $(window).on('load', function() {
-	// Resume navigation (dots)
-	$('#resume').scrollLeft(0);
-	history.scrollRestoration = "manual";
-
+	const bars = $('.bars a');
 	const resumeContainer = $('#resume');
-	const dots = $('.dots a');
+	history.scrollRestoration = "manual";
 	let moduleWidth = calculateModuleWidth();
+
+	resumeContainer.scrollLeft(0);
+
+	function calculateModuleWidth() {
+		const width = $('.module').eq(0).width();
+		return width;
+	}
+
+	function updateActiveDot(activeIndex) {
+		bars.removeAttr('id');
+		bars.eq(activeIndex).attr('id', 'active');
+	}
+
+	// Mouse/Touch controls
+	bars.on('click', function(e) {
+		bars.removeAttr('id');
+		$(this).attr('id', 'active');
+	});
+
+	resumeContainer.on('click', function() {
+		resumeContainer.focus();
+		resumeContainer.attr('tabindex', 0);
+	});
+
+	let isDragging = false;
+	let startX, scrollLeft, animationFrame;
+
+	resumeContainer.on('mousedown', function(e) {
+		isDragging = true;
+		startX = e.clientX - resumeContainer.offset().left;
+		scrollLeft = resumeContainer.scrollLeft();
+	});
+
+	$(document).on('mousemove', function(e) {
+		if (!isDragging) { return; }
+		e.preventDefault();
+		const x = e.clientX - resumeContainer.offset().left;
+		const walk = (x - startX) * 8;
+		// Slow animation to refresh rate
+		cancelAnimationFrame(animationFrame);
+		animationFrame = requestAnimationFrame(function() {
+			resumeContainer.scrollLeft(scrollLeft - walk);
+		});
+	});
+
+	$(document).on('mouseup', function() {
+		isDragging = false;
+	}).on('mouseleave', function() {
+		isDragging = false;
+	});
 
 	resumeContainer.on('scroll touchmove', function() {
 		const scrollPosition = resumeContainer.scrollLeft();
@@ -15,41 +63,44 @@ $(window).on('load', function() {
 		updateActiveDot(middleModuleIndex);
 	});
 
-	let isDragging = false;
-	let startX, scrollLeft;
+	// Keyboard controls
+	let arrowKeyPressed = false;
+	let spacebarPressed = false;
 
-	resumeContainer.on('mousedown', function(e) {
-		isDragging = true;
-		startX = e.clientX - resumeContainer.offset().left;
-		scrollLeft = resumeContainer.scrollLeft();
+	resumeContainer.on('keydown', function(e) {
+		const currentScrollLeft = resumeContainer.scrollLeft();
+		const containerWidth = resumeContainer.innerWidth();
+		const moduleCount = Math.floor(containerWidth / moduleWidth);
+		const scrollDistance = moduleWidth * moduleCount * 0.32;
+
+		if (e.key === 'ArrowLeft') {
+			e.preventDefault();
+			arrowKeyPressed = true;
+			resumeContainer.scrollLeft(currentScrollLeft - scrollDistance);
+		} else if (e.key === 'ArrowRight') {
+			e.preventDefault();
+			arrowKeyPressed = true;
+			resumeContainer.scrollLeft(currentScrollLeft + scrollDistance);
+		}
+
+		if ((e.key === ' ' || e.key === 'Spacebar') && !spacebarPressed) {
+			e.preventDefault();
+			if (!isDragging && !arrowKeyPressed) {
+				const checkboxes = $('input[type="checkbox"]');
+				const containerWidth = resumeContainer.innerWidth();
+				const snappedModuleIndex = Math.floor(resumeContainer.scrollLeft() / moduleWidth);
+				const middleModuleIndex = Math.floor(snappedModuleIndex + containerWidth / (2 * moduleWidth));
+				const checkboxToToggle = checkboxes.eq(middleModuleIndex);
+				if (checkboxToToggle.length > 0) {
+					checkboxToToggle.prop('checked', !checkboxToToggle.prop('checked'));
+				}
+			}
+			arrowKeyPressed = false;
+			spacebarPressed = true;
+		}
 	});
 
-	$(document).on('mousemove', function(e) {
-		if (!isDragging) return;
-		e.preventDefault();
-		const x = e.clientX - resumeContainer.offset().left;
-		const walk = (x - startX) * 8;
-		resumeContainer.scrollLeft(scrollLeft - walk);
-	});
-
-	$(document).on('mouseup', function() {
-		isDragging = false;
-	}).on('mouseleave', function() {
-		isDragging = false;
-	});
-
-	function calculateModuleWidth() {
-		const width = $('.module').eq(0).width();
-		return width;
-	}
-
-	function updateActiveDot(activeIndex) {
-		dots.removeAttr('id');
-		dots.eq(activeIndex).attr('id', 'active');
-	}
-
-	dots.on('click', function(e) {
-		dots.removeAttr('id');
-		$(this).attr('id', 'active');
+	resumeContainer.on('keyup', function(e) {
+		if (e.key === ' ' || e.key === 'Spacebar') { spacebarPressed = false; }
 	});
 });
