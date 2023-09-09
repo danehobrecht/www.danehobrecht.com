937cc84cdc23cd224eabaa6ac8569e262300d3ce
danehobrecht <76x5l22l@anonaddy.me>
2023-09-09 16:48:11 -0700

    modified:   assets/images/icons/dane-hobrecht-icon-001.svg 	new file:   assets/images/icons/dane-hobrecht-icon-005.svg 	modified:   index.html 	modified:   main.js 	modified:   styles.css

--- main.js
+++ main.js
@@ -1,102 +1,24 @@
-// Module navigation
 $(window).on('load', function() {
-	const bars = $('.bars a');
 	const resumeContainer = $('#resume');
+	let startX, scrollLeft, isDragging = false;
 
-	let isDragging = false;
-	let arrowKeyPressed = false;
-	let spacebarPressed = false;
-	let startX, scrollLeft, animationFrame;
-	let moduleWidth = calculateModuleWidth();
-
+  	// Reset position (Chrome)
 	resumeContainer.scrollLeft(0);
-	history.scrollRestoration = "manual";
 
-	// Calculative functions
-	function calculateModuleWidth() {
-		const width = $('.module').eq(0).width();
-		return width;
-	}
-
-	function updateActiveDot(activeIndex) {
-		bars.removeAttr('id');
-		bars.eq(activeIndex).attr('id', 'active');
-	}
-
-	// Mouse/Touch controls
-	bars.on('click', function() {
-		bars.removeAttr('id');
-		$(this).attr('id', 'active');
-	});
-
-	resumeContainer.on('click', function() {
-		resumeContainer.focus();
-		resumeContainer.attr('tabindex', 0);
-	});
-
-	resumeContainer.on('mousedown', function(e) {
+	resumeContainer.on('mousedown', (e) => {
 		isDragging = true;
 		startX = e.clientX - resumeContainer.offset().left;
 		scrollLeft = resumeContainer.scrollLeft();
 	});
 
-	$(document).on('mousemove', function(e) {
-		if (!isDragging) { return; }
+	$(document).on('mousemove', (e) => {
+		if (!isDragging) return;
 		e.preventDefault();
 		const x = e.clientX - resumeContainer.offset().left;
-		const walk = (x - startX) * 8;
-		// Slow animation to refresh rate
-		cancelAnimationFrame(animationFrame);
-		animationFrame = requestAnimationFrame(function() {
-			resumeContainer.scrollLeft(scrollLeft - walk);
-		});
+		resumeContainer.scrollLeft(scrollLeft - (x - startX) * 8);
 	});
 
-	$(document).on('mouseup', function() {
+	$(document).on('mouseup mouseleave', () => {
 		isDragging = false;
-	}).on('mouseleave', function() {
-		isDragging = false;
-	});
-
-	resumeContainer.on('scroll touchmove', function() {
-		const scrollPosition = resumeContainer.scrollLeft();
-		const containerWidth = resumeContainer.innerWidth();
-		const middleModuleIndex = Math.floor(scrollPosition / moduleWidth + containerWidth / (2 * moduleWidth));
-		updateActiveDot(middleModuleIndex);
-	});
-
-	// Keyboard controls
-	resumeContainer.on('keydown', function(e) {
-		const currentScrollLeft = resumeContainer.scrollLeft();
-		const containerWidth = resumeContainer.innerWidth();
-		const moduleCount = Math.floor(containerWidth / moduleWidth);
-		const scrollDistance = moduleWidth * moduleCount * 0.32;
-
-		if (e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
-			e.preventDefault();
-			arrowKeyPressed = true;
-			const scrollDirection = e.key === 'ArrowLeft' ? -1 : 1;
-			resumeContainer.scrollLeft(currentScrollLeft + scrollDirection * scrollDistance);
-		}
-
-		if ((e.key === ' ' || e.key === 'Spacebar') && !spacebarPressed) {
-			e.preventDefault();
-			if (!isDragging && !arrowKeyPressed) {
-				const checkboxes = $('input[type="checkbox"]');
-				const containerWidth = resumeContainer.innerWidth();
-				const snappedModuleIndex = Math.floor(resumeContainer.scrollLeft() / moduleWidth);
-				const middleModuleIndex = Math.floor(snappedModuleIndex + containerWidth / (2 * moduleWidth));
-				const checkboxToToggle = checkboxes.eq(middleModuleIndex);
-				if (checkboxToToggle.length > 0) {
-					checkboxToToggle.prop('checked', !checkboxToToggle.prop('checked'));
-				}
-			}
-			arrowKeyPressed = false;
-			spacebarPressed = true;
-		}
-	});
-
-	resumeContainer.on('keyup', function(e) {
-		if (e.key === ' ' || e.key === 'Spacebar') { spacebarPressed = false; }
 	});
 });
